59709daaf634b31a428e463d9dabf0bbc3385fd0
Alejandro <alevaldiviezo@gmail.com>
2023-10-10 09:53:23 +0100

    initial commit

---
+++ src/TodoForm.jsx
@@ -0,0 +1,38 @@
+import { IconButton, InputAdornment, ListItem, TextField } from "@mui/material";
+import { useState } from "react";
+import Create from "@mui/icons-material/Create";
+
+export default function TodoForm({addTodo}) {
+    const [text, setText] = useState("");
+    const handleChange = (e) => {
+        setText(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTodo(text);
+        setText("");
+    };
+    return(
+        <ListItem>
+            <form onSubmit={handleSubmit}>  
+                <TextField 
+                id="outlined-basic" 
+                label="Add to-do" 
+                variant="outlined"
+                onChange={handleChange}
+                value={text}
+                InputProps={{
+                    endAdornment:(
+                     <InputAdornment position="end">
+                        <IconButton aria-label="new todo" edge="end" type="submit">
+                            <Create />
+                        </IconButton>
+                     </InputAdornment>
+                    )
+                }}
+                />
+                </form>
+        </ListItem>
+    )
+}
